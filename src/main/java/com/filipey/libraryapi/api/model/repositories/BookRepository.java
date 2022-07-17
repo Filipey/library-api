8fedcb8c1f9689129804e9f5def6b3e5ac055304
Filipe Moura <filipeasm18@gmail.com>
2022-07-17 18:50:07 -0300

    feat: created BookJpaRepository

--- src/main/java/com/filipey/libraryapi/api/model/repositories/BookRepository.java
+++ src/main/java/com/filipey/libraryapi/api/model/repositories/BookRepository.java
@@ -1,2 +1,7 @@
-package com.filipey.libraryapi.api.model.repositories;public interface BookRepository {
+package com.filipey.libraryapi.api.model.repositories;
+
+import com.filipey.libraryapi.api.model.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
 }
