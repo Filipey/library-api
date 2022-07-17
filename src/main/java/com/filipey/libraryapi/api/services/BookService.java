e6bdf64607583af5f5542c09e66a49c7b6300e5d
Filipe Moura <filipeasm18@gmail.com>
2022-07-17 18:16:21 -0300

    feat: created BookService

--- src/main/java/com/filipey/libraryapi/api/services/BookService.java
+++ src/main/java/com/filipey/libraryapi/api/services/BookService.java
@@ -1,2 +1,10 @@
-package com.filipey.libraryapi.api.services;public interface BookService {
+package com.filipey.libraryapi.api.services;
+
+import com.filipey.libraryapi.api.model.entity.Book;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface BookService {
+
+    Book save(Book any);
 }
