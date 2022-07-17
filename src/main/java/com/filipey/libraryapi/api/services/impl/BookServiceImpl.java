7231884a341e0c2c142b8eb2e4d7a4c6c0cfeb5d
Filipe Moura <filipeasm18@gmail.com>
2022-07-17 18:49:47 -0300

    feat: created BookServiceImpl

--- src/main/java/com/filipey/libraryapi/api/services/impl/BookServiceImpl.java
+++ src/main/java/com/filipey/libraryapi/api/services/impl/BookServiceImpl.java
@@ -1,2 +1,21 @@
-package com.filipey.libraryapi.api.services.impl;public class BookServiceImpl {
+package com.filipey.libraryapi.api.services.impl;
+
+import com.filipey.libraryapi.api.model.entity.Book;
+import com.filipey.libraryapi.api.model.repositories.BookRepository;
+import com.filipey.libraryapi.api.services.BookService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    private final BookRepository repository;
+
+    public BookServiceImpl(BookRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Book save(Book book) {
+        return repository.save(book);
+    }
 }
